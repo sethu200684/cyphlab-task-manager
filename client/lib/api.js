d68953e8e51eab0968c511ca352015c9e8da0b24
Sethumdee <narampanawa2006@gmail.com>
2026-07-13 13:21:30 +0530

    Use environment variable for API URL

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
 
 export async function apiFetch(path, { method = "GET", body, token } = {}) {
   const res = await fetch(`${API_URL}${path}`, {
